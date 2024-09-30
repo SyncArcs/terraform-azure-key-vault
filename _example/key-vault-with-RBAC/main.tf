5e977994ca9b8875f0c1a6298e2e6989633e3c3a
ROHIT YADAV <rohit9950518156@gmail.com>
2024-09-30 10:31:14 +0530

    Initial commit

---
+++ _example/key-vault-with-RBAC/main.tf
@@ -0,0 +1,64 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source      = "git::https://github.com/SyncArcs/terraform-azure-resource-group.git?ref=v1.0.0"
+  name        = "app"
+  environment = "test"
+  location    = "North Europe"
+}
+
+#Vnet
+module "vnet" {
+  source              = "git::https://github.com/SyncArcs/terraform-azure-vnet.git?ref=v1.0.0"
+  name                = "app"
+  environment         = "test"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.0.0.0/16"
+}
+
+module "subnet" {
+  source               = "git::https://github.com/SyncArcs/terraform-azure-subnet.git?ref=v1.0.0"
+  name                 = "app2"
+  environment          = "test2"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = module.vnet.name
+
+  #subnet
+  subnet_names    = ["subnet1"]
+  subnet_prefixes = ["10.0.1.0/24"]
+
+  # route_table
+  enable_route_table = true
+  route_table_name   = "default_subnet"
+  routes = [
+    {
+      name           = "rt-test"
+      address_prefix = "0.0.0.0/0"
+      next_hop_type  = "Internet"
+    }
+  ]
+}
+
+#Key Vault
+module "vault" {
+  source                      = "./../.."
+  name                        = "annoh64dhyr"
+  environment                 = "test"
+  sku_name                    = "standard"
+  principal_id                = ["xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"]
+  role_definition_name        = ["Key Vault Administrator"]
+  resource_group_name         = module.resource_group.resource_group_name
+  subnet_id                   = module.subnet.default_subnet_id
+  virtual_network_id          = module.vnet.id
+  enable_private_endpoint     = true
+  enable_rbac_authorization   = true
+  purge_protection_enabled    = false
+  enabled_for_disk_encryption = true
+
+  depends_on = [module.resource_group, module.vnet]
+}
+
---
+++ _example/key-vault-with-access-policy/main.tf
@@ -0,0 +1,106 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source      = "git::https://github.com/SyncArcs/terraform-azure-resource-group.git?ref=v1.0.0"
+  name        = "demo"
+  environment = "test"
+  location    = "North Europe"
+}
+
+module "vnet" {
+  source              = "git::https://github.com/SyncArcs/terraform-azure-vnet.git?ref=v1.0.0"
+  name                = "demo"
+  environment         = "test"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.0.0.0/16"
+}
+
+
+module "subnet" {
+  source               = "git::https://github.com/SyncArcs/terraform-azure-subnet.git?ref=v1.0.0"
+  name                 = "demo"
+  environment          = "test"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = module.vnet.name
+
+  #subnet
+  subnet_names    = ["subnet1new"]
+  subnet_prefixes = ["10.0.1.0/24"]
+
+  # route_table
+  enable_route_table = true
+  route_table_name   = "default_subnet"
+  routes = [
+    {
+      name           = "rt-test"
+      address_prefix = "0.0.0.0/0"
+      next_hop_type  = "Internet"
+    }
+  ]
+}
+
+#Key Vault
+module "vault" {
+  source                      = "./../.."
+  name                        = "ann14hdc"
+  environment                 = "test"
+  resource_group_name         = module.resource_group.resource_group_name
+  purge_protection_enabled    = false
+  enabled_for_disk_encryption = true
+  sku_name                    = "standard"
+  subnet_id                   = module.subnet.default_subnet_id
+  virtual_network_id          = module.vnet.id
+  enable_private_endpoint     = true
+
+  depends_on = [module.resource_group, module.vnet]
+  access_policy = [
+    {
+      object_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      key_permissions = [
+        "Get",
+        "List",
+        "Update",
+        "Create",
+        "Import",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+        "UnwrapKey",
+        "WrapKey",
+        "GetRotationPolicy"
+      ]
+      certificate_permissions = [
+        "Get",
+        "List",
+        "Update",
+        "Create",
+        "Import",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+        "ManageContacts",
+        "ManageIssuers",
+        "GetIssuers",
+        "ListIssuers",
+        "SetIssuers",
+        "DeleteIssuers"
+      ]
+      secret_permissions = [
+        "Get",
+        "List",
+        "Set",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore"
+      ]
+      storage_permissions = []
+    },
+  ]
+}
